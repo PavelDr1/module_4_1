fb2cb6a9e72fceaa3189ed1997c3d9a512ece5f6
PavelDr1 <pawkane@gmail.com>
2024-10-29 16:56:54 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -2,6 +2,6 @@ from math import inf
 
 def divide (first, second):
     if second == 0:
-        return 'в бесконечность и выше!'
+        return 'в бесконечность и далее! )'
     else:
         return first/second
